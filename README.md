4089a0e4db29913007b94b3072b2da0b60f356b7
paigexx <pcjones12@gmail.com>
2024-02-01 13:20:58 -0600

    inital commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# A redirect frame example
+
+Built using [a-frame-in-100-lines](https://github.com/Zizzamia/a-frame-in-100-lines) by [Zizzamia](https://github.com/Zizzamia)
+
+## Resources
+
+- [Official Farcaster Frames docs](https://warpcast.notion.site/Farcaster-Frames-4bd47fe97dc74a42a48d3a234636d8c5)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+      {label: 'Play Beta', action: 'post_redirect'}
+  ],
+  image: 'https://redirect-frame.vercel.app/cosmiccowboys.png',
+  post_url: 'https://redirect-frame.vercel.app/api/frame',
+});
+
+export const metadata: Metadata = {
+  title: 'Redirect frame',
+  description: 'LFG',
+  openGraph: {
+    title: 'Redirect frame',
+    description: 'LFG',
+    images: ['https://redirect-frame.vercel.app/cosmiccowboys.png'],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <img src={'/video.png'}/>
+    </>
+  );
+}
